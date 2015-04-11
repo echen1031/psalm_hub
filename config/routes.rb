0240e42527e2236f875e96ebac7fceeb8115a4a0
Eric Chen <eric87chen@gmail.com>
2015-04-11 18:09:02 -0400

    delete a get method

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   post "songs/upload", :as => "upload"
-  post "songs/delete", :as => "delete"
+  get "songs/delete", :as => "delete"
 
   #get 'songs/index'
   root 'songs#index'
