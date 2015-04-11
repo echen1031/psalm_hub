614ddc9aa8a0f26daf76489f136b58bf63f0efc2
Eric Chen <eric87chen@gmail.com>
2015-04-11 17:59:37 -0400

    Changed route get to post, added upload form to index, upload controller

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -4,6 +4,12 @@ class SongsController < ApplicationController
   end
 
   def upload
+    begin
+      AWS::S3::S3Object.store(sanitize_filename(params[:mp3file].original_filename), params[:mp3file].read, ENV["AWS_BUCKET_NAME"], :access => :public_read)
+      redirect_to root_path
+    rescue
+      render :text => "Couldn't complete the upload"
+    end
   end
 
   def delete
@@ -13,6 +19,13 @@ class SongsController < ApplicationController
     else
       render :text => "No song was found to delete!"
     end
+  end
+
+  private
+
+  def sanitize_filename(file_name)
+    just_filename = File.basename(file_name)
+    just_filename.sub(/[^\w\.\-]/,'_')
   end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get "songs/upload", :as => "upload"
-  get "songs/delete", :as => "delete"
+  post "songs/upload", :as => "upload"
+  post "songs/delete", :as => "delete"
 
   #get 'songs/index'
   root 'songs#index'
