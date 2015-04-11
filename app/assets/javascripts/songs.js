09c71cd4f2f591816d6f4a2ae6d8f659174a2675
Eric Chen <eric87chen@gmail.com>
2015-04-11 18:31:17 -0400

    Able to download and stream songs

--- app/assets/javascripts/songs.js
+++ app/assets/javascripts/songs.js
@@ -1,2 +1,16 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(document).ready(function() {
+  var audioSection = $('section#audio');
+  $('a.html5').click(function() {
+
+    var audio = $('<audio>', {
+      controls : 'controls'
+    });
+
+    var url = $(this).attr('href');
+    $('<source>').attr('src', url).appendTo(audio);
+    audioSection.html(audio);
+    return false;
+  });
+});
