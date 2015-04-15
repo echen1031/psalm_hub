73ef47ef5b749128dd93aa4b53d67076b52219c0
guy cao <guy.cao@bfa.org>
2015-04-15 14:37:24 -0700

    update seeds file with link

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,11 +2,14 @@ puts "seeding songs..."
 Song.delete_all
 Song.create!(
   [
-    { id: 1,
+    {
+      id: 1,
       title: "I am the living bread",
       lyrics: "I am the living bread, which came down out of heaven",
       chords: "G C Em A",
-      link: "http://s3.amazonaws.com/psalmhubeast/I_Am%20The%20Living%20Bread.mp3" }
+      link: "http://s3.amazonaws.com/psalmhubeast/I_Am%20The%20Living%20Bread.mp3"
+    }
   ]
 )
+
 
