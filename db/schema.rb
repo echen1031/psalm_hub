566edf47f7254e2a98dcd8e72ff911766c25de64
guy cao <guy.cao@bfa.org>
2015-04-14 21:06:55 -0700

    ran migration for song model

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150404221944) do
+
+  create_table "songs", force: true do |t|
+    t.string   "title"
+    t.string   "lyrics"
+    t.string   "chords"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
