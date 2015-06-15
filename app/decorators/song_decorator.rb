71a8a84a11e83693b77163279a392d78e246daab
Eric Chen <eric87chen@gmail.com>
2015-06-15 09:15:40 -0400

    Added draper

---
+++ app/decorators/song_decorator.rb
@@ -0,0 +1,15 @@
+class SongDecorator < Draper::Decorator
+  delegate_all
+
+  def number_of_lines
+    lyrics.split('|').count
+  end
+
+  def display_chords
+    self.chords.split('|')
+  end
+
+  def display_lyrics
+    self.lyrics.split('|')
+  end
+end
