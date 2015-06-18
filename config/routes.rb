61cb8baab2e834e7bc9d4cd05ff5763b463f6b9d
Eric Chen <eric87chen@gmail.com>
2015-06-18 00:54:44 -0400

    BibleVerse controller and views for #index and @show

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  resources :bible_verses
 
   post "songs/upload", :as => "upload"
   get "songs/delete", :as => "delete"
 
   resources :songs, only: [:show, :index, :new, :create, :edit, :update]
+  resources :bible_verses, only: [:show, :index]
 
   root 'songs#index'
 end
